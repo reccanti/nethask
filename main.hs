fe7fc4f6640ad8b0fcfa6fc0932599f0e5985082
Ben Wilcox <reccanti@gmail.com>
2019-02-03 01:58:22 -0500

    Allow the user to move
    Allows the user to move using standard vim keys

--- main.hs
+++ main.hs
@@ -3,12 +3,16 @@
 
 -- Goal #1 - Create a map that the user can walk around
 -- [X] Create and render a map
--- [] Render the user at the starting location
--- [] Update the user's location when the input changes
+-- [X] Render the user at the starting location
+-- [X] Update the user's location when the input changes
+import Control.Monad.State.Lazy
+
 data GameState = GameState {
     userLocation :: (Int, Int),
     roomDimensions :: (Int, Int)
 } deriving (Show)
+
+data UserAction = MoveUp | MoveDown | MoveLeft | MoveRight deriving (Show)
 
 -- Display a grid with the given dimensions
 renderGrid :: GameState -> String
@@ -24,20 +28,58 @@ renderPlayerOnGrid gs grid =
         index = (width + 1) * y + x
     in replace grid '*' index
     where
+        replace :: String -> Char -> Int -> String
         replace grid char index = 
             let (x,_:xs) = splitAt index grid
             in x ++ [char] ++ xs
 
+-- Given an input Char, convert it into a UserAction
+getUserAction :: Char -> Maybe UserAction
+getUserAction c =
+    case c of
+        'h' -> Just MoveLeft
+        'j' -> Just MoveDown
+        'k' -> Just MoveUp
+        'l' -> Just MoveRight
+        _ -> Nothing
+        
+-- Given a UserAction, update the state
+updateStateWithAction :: UserAction -> State GameState Bool
+updateStateWithAction action = do
+    gs <- get
+    let (x, y) = userLocation gs
+    let (width, height) = roomDimensions gs
+    case action of
+        MoveUp | y > 0 -> put $ gs { userLocation=(x, y-1) }
+        MoveDown | y < (height-1) -> put $ gs { userLocation=(x, y+1) }
+        MoveRight | x < (width-1) -> put $ gs { userLocation=(x+1, y) }
+        MoveLeft | x > 0 -> put $ gs { userLocation=(x-1, y) }
+        _ -> put gs
+    return True
+
+
 main = do
     state <- initialize
-    render state
+    loop state
     where
         initialize :: IO GameState
         initialize = do
+            putStrLn "initializing"
             return GameState {
                 userLocation=(5,5),
                 roomDimensions=(10,10)
             }
+        loop :: GameState -> IO ()
+        loop gs = do
+            -- -- Update the State with the input
+            (x:_) <- getLine
+            let mAction = getUserAction x
+            let (continue, newState) = case mAction of
+                                        Just action -> (runState (updateStateWithAction action) gs)
+                                        Nothing -> (True, gs)
+            -- Render the screen based on the State
+            render newState
+            if continue then loop newState else return ()
         render :: GameState -> IO ()
         render gs = do
             let grid = renderPlayerOnGrid gs (renderGrid gs)
